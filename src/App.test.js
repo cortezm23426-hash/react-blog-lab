a72374edebf894b3721bc003633209cf8a2bb6dc
Ashley Gerber <cortezm23426@hotmail.com>
2026-03-16 13:34:34 -0400

    Fix App test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from "@testing-library/react"
+import React from "react"
 import App from "./App"
 
 test("renders blog title", () => {
-  render(<App />)
+  render(React.createElement(App))
   const heading = screen.getByText(/blog/i)
   expect(heading).toBeInTheDocument()
 })
