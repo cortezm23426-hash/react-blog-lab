0f399fcad0fa4515027f26d2304235952f988f09
Ashley Gerber <cortezm23426@hotmail.com>
2026-03-16 13:43:46 -0400

    Fix JSX extensions

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import { render, screen } from "@testing-library/react"
 import React from "react"
-import App from "./App"
+import App from "./App.jsx"
+import { test, expect } from "vitest"
 
 test("renders blog title", () => {
   render(React.createElement(App))
   const heading = screen.getByText(/blog/i)
-  expect(heading).toBeInTheDocument()
+  expect(heading).toBeTruthy()
 })
